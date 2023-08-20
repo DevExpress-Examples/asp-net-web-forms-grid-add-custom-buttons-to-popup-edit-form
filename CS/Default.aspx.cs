3f84a3eb4e09b7217e88702f3ff5b0bd066110fd
Svetlana Nikulina <svetlana.nikulina@devexpress.com>
2023-08-20 14:03:27 +0400

    Update Default.aspx.cs

--- CS/Default.aspx.cs
+++ CS/Default.aspx.cs
@@ -6,20 +6,13 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using DevExpress.Web;
 
-public partial class _Default : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
-    protected void buttonPrev_Init(object sender, EventArgs e)
-    {
+public partial class _Default : System.Web.UI.Page {
+    protected void buttonPrev_Init(object sender, EventArgs e) {
         ASPxButton button = sender as ASPxButton;
         GridViewDataItemTemplateContainer container = button.NamingContainer as GridViewDataItemTemplateContainer;
         button.ClientSideEvents.Click = string.Format("function(s, e){{ gridView.StartEditRow({0}-1); }}", container.VisibleIndex);
     }
-    protected void buttonNext_Init(object sender, EventArgs e)
-    {
+    protected void buttonNext_Init(object sender, EventArgs e) {
         ASPxButton button = sender as ASPxButton;
         GridViewDataItemTemplateContainer container = button.NamingContainer as GridViewDataItemTemplateContainer;
         button.ClientSideEvents.Click = string.Format("function(s, e){{ gridView.StartEditRow({0}+1); }}", container.VisibleIndex);
